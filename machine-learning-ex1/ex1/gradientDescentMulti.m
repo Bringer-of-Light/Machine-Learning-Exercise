bf1ce710e9ab3d0fa9b67998f3fa8420e36db166
Bringer-of-Light <sunbo.seo@outlook.com>
2017-09-08 17:22:07 +0800

    mod exercise
    1. 改进梯度下降核心方法为全向量化代码

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,12 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-    temp = theta;
-    for j=1:length(theta)
-      temp(j) = theta(j) - (alpha/m)*sum((X*theta-y).*X(:,j));
-    end
-    theta = temp;
+    
+    theta = theta - (alpha/m)*X'*(X*theta-y);
 
     % ============================================================
 
